1a83e445828bb6ed0e9ebecf9c11efed2a130c0c
VICENTE NASCIMENTO <vicente19981@live.com>
2020-04-30 17:04:36 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [numBinary, setnumBinary] = useState('');
+  const [numDec, setnumDec] = useState('');
+
+
+  const onFormSubmit = e => {
+    e.preventDefault();
+
+    const reservarBinary = numBinary
+      .split('')
+      .map(Number)
+      .reverse()
+
+    const result = reservarBinary.reduce(
+      (accumulator, currentValue, idx) =>
+        accumulator + currentValue * Math.pow(2, idx)
+    )
+    setnumDec(result)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="container"> 
+     
+        <form onSubmit={onFormSubmit}>
+        <h1>Binary to Decimal</h1>
+        <label>Binary Input</label>
+          <input
+            type="text"
+            placeholder="Entre com 0 ou 1"
+            required
+            pattern="[0-1]+$"
+            value={numBinary}
+            onChange={e => { setnumBinary(e.target.value) }}
+          />
+          <button type="submit">Convert</button>
+          <label>Decimal Output</label>
+          <input
+            value={numDec}
+            type="text"
+            disabled
+          />
+        </form>
+      </div>
+    </>
+  )
 }
 
 export default App;
